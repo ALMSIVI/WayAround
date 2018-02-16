ef3f78a5d3b2f6ba44a3d5d9fffc50d2576ae9f9
ALMSIVI <zakarumbeliever@hotmail.com>
2018-02-15 20:48:40 -0800

    Fixed exceptions, modified style of buttons in review page

--- routes/actions.js
+++ routes/actions.js
@@ -1,7 +1,12 @@
+//History data
+var history = require("../json/history.json");
+
+var data = require("../json/profile.json");
+
 exports.history = function(req, res) {
-	res.render('history');
+	res.render('history', history);
 }
 
 exports.profile = function(req, res) {
-	res.render('profile');
+	res.render('profile', data);
 }
--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,11 @@
 /*
  * GET home page.
  */
-//History data
-var history = require('history.json');
-
-var data = require('profile.json');
 
 exports.login = function(req, res) {
 	res.redirect('login');
 }
 
 exports.view = function(req, res) {
-	res.render('index',history);
+	res.render('index');
 };

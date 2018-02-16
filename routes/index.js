b0ad5eac8ed08ff6bede65bb6511c1f40d5bf72e
germann12346 <german_pelayoo@yahoo.com>
2018-02-15 20:27:44 -0800

    history

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,15 @@
 /*
  * GET home page.
  */
+//History data
+var data = require('history.json');
+
+var data = require('profile.json');
 
 exports.login = function(req, res) {
 	res.redirect('login');
 }
 
 exports.view = function(req, res) {
-	res.render('index');
+	res.render('index',history);
 };

cb619207befad517fe8ecc0931ba2f2f6ba88914
ALMSIVI <zakarumbeliever@hotmail.com>
2018-02-15 21:56:24 -0800

    Modified title

--- routes/thankyou.js
+++ routes/thankyou.js
@@ -1,3 +1,11 @@
 exports.show = function(req, res) {
-    res.render('thankyou');
+	console.log(req.body);
+
+	var fs = require('fs');
+	fs.writeFile("./json/review.json", JSON.stringify(req.body), function(err) {
+		if(err) {
+			return console.log(err);
+		}
+	});
+	res.render('thankyou');
 };
